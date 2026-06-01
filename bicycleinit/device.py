3c2cbb91816cef2e8173dbdb2b6676cbe18c4d35
David Bratu <David.Bratu@vti.se>
2026-06-01 15:08:19 +0200

    Add debug mode

--- bicycleinit/device.py
+++ bicycleinit/device.py
@@ -108,6 +108,9 @@ class BicycleDevice:
     # LED2 indicates GPS status
     # LED3 indicates Radar status
 
+    bicyclebutton.start_bicyclebutton('bicyclebutton', {'session': self.session, 'gpio': 23, 'upload_interval': 300})
+    debug_mode = bicyclebutton._button.is_pressed
+    
     # Enable wifi
     time.sleep(1)
     bluetooth.off()
@@ -172,8 +175,9 @@ class BicycleDevice:
           logging.info(f"Installed requirements for {sensor['name']} from {req_path}")
         except Exception as e:
           logging.error(f"Failed to install requirements for {sensor['name']} from {req_path}: {e}")
-
-    self.stop_wifi()  # Disable WiFi to save power until needed again
+    
+    if not debug_mode:
+      self.stop_wifi()  # Disable WiFi to save power until needed again
 
     time.sleep(1)
     bluetooth.on()
@@ -186,7 +190,7 @@ class BicycleDevice:
       sensor['args']['session'] = self.session
       sensor_manager.start_sensor(sensor['name'], 'sensors.' + sensor['name'] + "." + file, fcn, sensor['args'])
 
-    bicyclebutton.start_bicyclebutton('bicyclebutton', {'session': self.session, 'gpio': 23, 'upload_interval': 300})
+    # bicyclebutton.start_bicyclebutton('bicyclebutton', {'session': self.session, 'gpio': 23, 'upload_interval': 300})
 
     while True:
       # Wait for any connection to become ready
